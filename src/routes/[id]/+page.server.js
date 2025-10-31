0665cb7d5f41ac42b3dec5a49088f525f02da2f3
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-10-31 10:39:01 +0700

    final version

--- src/routes/[id]/+page.server.js
+++ src/routes/[id]/+page.server.js
@@ -1,25 +1,25 @@
-import { PUBLIC_SERVICE_ACC_EMAIL, PUBLIC_SERVICE_ACC_KEY, PUBLIC_SS_ID} from '$env/static/public';
+import {SERVICE_ACC_EMAIL, SERVICE_ACC_KEY, SS_ID} from '$env/static/private';
 import { google } from 'googleapis';
 
 export const prerender = true;
 
 const auth = new google.auth.JWT({
-  email: PUBLIC_SERVICE_ACC_EMAIL,
-  key: PUBLIC_SERVICE_ACC_KEY.replace(/\\n/g, '\n'), // Fix escaped newlines
+  email: SERVICE_ACC_EMAIL,
+  key: SERVICE_ACC_KEY.replace(/\\n/g, '\n'), // Fix escaped newlines
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
 
 export async function load({params}) {
     const sheets = google.sheets({ version: 'v4', auth });
     const { data: indices } = await sheets.spreadsheets.values.get({
-      spreadsheetId: PUBLIC_SS_ID,
+      spreadsheetId: SS_ID,
       range: 'qr!A:A',
     });
 
     // first we find the index of the row first
     const rowIndex = indices.values.findIndex(([row]) => row === params.id);
     const { data } = await sheets.spreadsheets.values.batchGet({
-      spreadsheetId: PUBLIC_SS_ID,
+      spreadsheetId: SS_ID,
       ranges: [
         `qr!A1:Z1`,
         `qr!A${rowIndex + 1}:Z${rowIndex + 1}`
